fffe53958f36069882e5290f412e065a009a761b
Hok Soy <100107761+SOYHOK@users.noreply.github.com>
2022-07-21 07:14:21 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  int num1, num2, Total;
+  printf("Enter the num: ");
+  scanf("%d%d",&num1,&num2);
+  Total= num1 +num2;
+  printf("Total is %d",Total);
+  return 0;
+}
